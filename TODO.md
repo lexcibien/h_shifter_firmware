2fc80ce9dbf1c66c202ad9a7ee039f8d7b1fd99d
Alex Luiz <65964826+lexcibien@users.noreply.github.com>
2026-01-22 22:06:49 -0300

    added todo tasks, reverse and truck buttons

--- TODO.md
+++ TODO.md
@@ -1,5 +1,17 @@
 # TODO List
 
+## Programação
+
 - [ ] Adicionar marcha ré
-- [ ] Adicionar switch para trocar de H para sequencial
-  - [ ] Adicionar dois botões diferente para o sequencial
+  - [ ] Talvez colocar um switch na base e um mecanismo na manopla que quando puxa e engata a 6a marcha, ativa a marcha ré, quando volta,
+esse sistema desengata
+- [ ] Fazer a lógica com o switch para habilitar/desabilitar câmbio sequencial
+  - [ ] Adicionar dois botões adicionais para o sequencial quando ativado (ver se dá de fazer condicional ou permanente, se ele perde
+a configuração no jogo)
+- [X] Adicionar botões da manopla do caminhão para usar ela junta
+  
+## Hardware
+
+- [ ] Reimprimir peças em PLA+
+- [ ] Colocar um conector no chicote para a manopla
+- [ ] Adicionar um switch para trocar de H para sequencial
--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,15 @@ const uint8_t PIN_BTN4 = 4;
 const uint8_t PIN_BTN5 = 5;
 const uint8_t PIN_BTN6 = 6;
 const uint8_t PIN_BTN7 = 7;
+const uint8_t PIN_BTN8 = 8; // Botão de marcha ré
+
+// Botões da manopla de caminhão
+const uint8_t PIN_BTN9 = 9;
+const uint8_t PIN_BTN10 = 10;
+const uint8_t PIN_BTN11 = 11;
 
 // Número de botões lógicos reportados pelo Joystick
-const uint8_t BUTTON_COUNT = 6;
+const uint8_t BUTTON_COUNT = 10; // Marchas (6 + R) e botões da manopla (3)
 
 // Estado anterior dos botões (índices 0..BUTTON_COUNT-1)
 bool prevButtonState[BUTTON_COUNT] = { 0 };
@@ -26,12 +32,19 @@ Joystick_ GameController(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
   false, false, false);
 
 void setup() {
-  Serial.begin(38400);
+  Serial.begin(115200);
+
   GameController.begin();
+
   pinMode(PIN_BTN4, INPUT_PULLUP);
   pinMode(PIN_BTN5, INPUT_PULLUP);
   pinMode(PIN_BTN6, INPUT_PULLUP);
   pinMode(PIN_BTN7, INPUT_PULLUP);
+  pinMode(PIN_BTN8, INPUT_PULLUP);
+
+  pinMode(PIN_BTN9, INPUT_PULLUP);
+  pinMode(PIN_BTN10, INPUT_PULLUP);
+  pinMode(PIN_BTN11, INPUT_PULLUP);
 }
 
 void loop() {
@@ -40,6 +53,11 @@ void loop() {
   bool btn5 = (digitalRead(PIN_BTN5) == LOW);
   bool btn6 = (digitalRead(PIN_BTN6) == LOW);
   bool btn7 = (digitalRead(PIN_BTN7) == LOW);
+  bool btn8 = (digitalRead(PIN_BTN8) == LOW);
+
+  bool btn9 = (digitalRead(PIN_BTN9) == LOW);
+  bool btn10 = (digitalRead(PIN_BTN10) == LOW);
+  bool btn11 = (digitalRead(PIN_BTN11) == LOW);
 
   // Calcula novo estado dos botões
   bool newButtonState[BUTTON_COUNT] = { 0 };
@@ -57,8 +75,16 @@ void loop() {
   if (btn4 && !comb4Used) newButtonState[2] = true;
   if (btn7 && !comb7Used) newButtonState[3] = true;
 
+  //TODO Sei que a marcha ré não funciona assim, pois precisa que btn8 esteja sempre pressionado, talvez fazer algo mecânico para resolver isso.
+  if (btn8 && comb4Used) newButtonState[6] = true;
+  
+  // Botões da manopla de caminhão
+  if (btn9) newButtonState[7] = true;
+  if (btn10) newButtonState[8] = true;
+  if (btn11) newButtonState[9] = true;
+
   // --- Atualiza somente se houver alteração ---
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < BUTTON_COUNT; i++) {
     if (newButtonState[i] != prevButtonState[i]) {
       GameController.setButton(i, newButtonState[i]);
       prevButtonState[i] = newButtonState[i];
