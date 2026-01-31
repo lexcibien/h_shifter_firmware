a972e0eafeeb18c38719fa271296e5d60a6bb8a0
Alex Luiz <65964826+lexcibien@users.noreply.github.com>
2026-01-31 18:23:03 -0300

    adding flag to assert it's leonard or usb compatible

--- src/main.cpp
+++ src/main.cpp
@@ -40,11 +40,7 @@ bool handleConnected = false;
 void setup() {
   Serial.begin(115200);
 
-#if ARDUINO_RASPBERRY_PI_PICO_2
-  GameController.begin();
-#elif ARDUINO_AVR_LEONARDO
   GameController.begin(initAutoSendState);
-#endif
 
   pinMode(PIN_BTN4, INPUT_PULLUP);
   pinMode(PIN_BTN5, INPUT_PULLUP);
@@ -69,14 +65,14 @@ void setup() {
   }
 
   if (!handleConnected) {
-    GameController.setButton(static_cast<uint8_t>(ControllerButtons::SW_SPLIT), 0);
-    GameController.setButton(static_cast<uint8_t>(ControllerButtons::SW_RANGE), 0);
-    GameController.setButton(static_cast<uint8_t>(ControllerButtons::BTN_ENGINE_BRAKE), 0);
+    GameController.setButton(static_cast<uint8_t>(ControllerButtons::SW_SPLIT), LOW);
+    GameController.setButton(static_cast<uint8_t>(ControllerButtons::SW_RANGE), LOW);
+    GameController.setButton(static_cast<uint8_t>(ControllerButtons::BTN_ENGINE_BRAKE), LOW);
   }
 }
 
 void loop() {
-  static bool prevButtonState[BUTTON_COUNT] = { false };
+  static bool prevButtonState[BUTTON_COUNT] = { LOW };
 
   bool btn4 = (digitalRead(PIN_BTN4) == LOW);
   bool btn5 = (digitalRead(PIN_BTN5) == LOW);
@@ -88,29 +84,29 @@ void loop() {
   bool btn10 = (digitalRead(PIN_BTN10) == LOW);
   bool btn11 = (digitalRead(PIN_BTN11) == LOW);
 
-  bool newButtonState[BUTTON_COUNT] = { false };
+  bool newButtonState[BUTTON_COUNT] = { LOW };
 
   bool comb4Used = false;
   bool comb7Used = false;
 
   // Combinações para marchas laterais
-  if (btn4 && btn5) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_1)] = true; comb4Used = true; }
-  if (btn5 && btn7) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_2)] = true; comb7Used = true; }
-  if (btn4 && btn6) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_5)] = true; comb4Used = true; }
-  if (btn6 && btn7) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_6)] = true; comb7Used = true; }
+  if (btn4 && btn5) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_1)] = HIGH; comb4Used = true; }
+  if (btn5 && btn7) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_2)] = HIGH; comb7Used = true; }
+  if (btn4 && btn6) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_5)] = HIGH; comb4Used = true; }
+  if (btn6 && btn7) { newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_6)] = HIGH; comb7Used = true; }
 
   // Marchas centrais
-  if (btn4 && !comb4Used) newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_3)] = true;
-  if (btn7 && !comb7Used) newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_4)] = true;
+  if (btn4 && !comb4Used) newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_3)] = HIGH;
+  if (btn7 && !comb7Used) newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_4)] = HIGH;
 
   //* Sei que a marcha ré não funciona assim, pois precisa que btn8 esteja sempre pressionado, talvez fazer algo mecânico para resolver isso.
-  if (btn8 && comb4Used) newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_R)] = true;
+  if (btn8 && comb4Used) newButtonState[static_cast<uint8_t>(ControllerButtons::GEAR_R)] = HIGH;
 
   // Botões da manopla de caminhão
   if (handleConnected) {
-    if (btn9) newButtonState[static_cast<uint8_t>(ControllerButtons::SW_SPLIT)] = true;
-    if (btn10) newButtonState[static_cast<uint8_t>(ControllerButtons::SW_RANGE)] = true;
-    if (btn11) newButtonState[static_cast<uint8_t>(ControllerButtons::BTN_ENGINE_BRAKE)] = true;
+    if (btn9) newButtonState[static_cast<uint8_t>(ControllerButtons::SW_SPLIT)] = HIGH;
+    if (btn10) newButtonState[static_cast<uint8_t>(ControllerButtons::SW_RANGE)] = HIGH;
+    if (btn11) newButtonState[static_cast<uint8_t>(ControllerButtons::BTN_ENGINE_BRAKE)] = HIGH;
   }
 
   for (uint8_t i = 0; i < BUTTON_COUNT; i++) {
