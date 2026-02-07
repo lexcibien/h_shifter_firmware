217c0c944685c04759b5e1cf731a5a3d7e88df20
Alex Luiz <65964826+lexcibien@users.noreply.github.com>
2026-02-07 00:21:53 -0300

    remapping pins

--- boards/pinouts/leonardo.h
+++ boards/pinouts/leonardo.h
@@ -4,15 +4,15 @@
 #include "Arduino.h"
 
 // Fim de curso do H-shifter - Vista de cima com a primeira marcha topo esquerda
-const uint8_t SW_FRONT = 4;
-const uint8_t SW_LEFT = 5;
-const uint8_t SW_RIGHT = 6;
-const uint8_t SW_BACK = 7;
-const uint8_t SW_REVERSE = 8;
+const uint8_t SW_FRONT = 3;
+const uint8_t SW_LEFT = 4;
+const uint8_t SW_RIGHT = 5;
+const uint8_t SW_BACK = 6;
+const uint8_t SW_REVERSE = 7;
 
 // Botões da manopla de caminhão
-const uint8_t SW_RANGE = 9;
-const uint8_t SW_SPLIT = 10;
-const uint8_t BTN_ENGINE_BRAKE = 11;
+const uint8_t SW_RANGE = 8;
+const uint8_t SW_SPLIT = 9;
+const uint8_t BTN_ENGINE_BRAKE = 10;
 
 #endif // PINS_ARDUINO_H
