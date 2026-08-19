cb31de76f7b8d66ff7b7d939f8bef9173fa3363a
Alex Luiz <alexci@hotmail.com>
2026-08-19 06:36:37 -0300

    format array

--- src/main.cpp
+++ src/main.cpp
@@ -26,18 +26,18 @@ struct __attribute__((packed)) ButtonsReport {
 };
 
 uint8_t const desc_hid_report[] = {
-    0x05, 0x01,  // Usage Page (Generic Desktop)
-    0x09, 0x05,  // Usage (Gamepad)
-    0xA1, 0x01,  // Collection (Application)
-    0x05, 0x09,  // Usage Page (Button)
-    0x19, 0x01,  // Usage Minimum (Button 1)
-    0x29, HID_BUTTON_COUNT,  // Usage Maximum (Button N)
-    0x15, 0x00,  // Logical Minimum (0)
-    0x25, 0x01,  // Logical Maximum (1)
-    0x75, 0x01,  // Report Size (1)
-    0x95, HID_BUTTON_COUNT,  // Report Count (N)
-    0x81, 0x02,  // Input (Data, Var, Abs)
-    0xC0,        // End Collection
+  0x05, 0x01,             // Usage Page (Generic Desktop)
+  0x09, 0x05,             // Usage (Gamepad)
+  0xA1, 0x01,             // Collection (Application)
+  0x05, 0x09,             // Usage Page (Button)
+  0x19, 0x01,             // Usage Minimum (Button 1)
+  0x29, HID_BUTTON_COUNT, // Usage Maximum (Button N)
+  0x15, 0x00,             // Logical Minimum (0)
+  0x25, 0x01,             // Logical Maximum (1)
+  0x75, 0x01,             // Report Size (1)
+  0x95, HID_BUTTON_COUNT, // Report Count (N)
+  0x81, 0x02,             // Input (Data, Var, Abs)
+  0xC0,                   // End Collection
 };
 
 Adafruit_USBD_HID usb_hid;
